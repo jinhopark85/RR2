9055b4e3fe7444b1a45e2c7cd463dfe2ca3e0eb0
ganeodolu <ganeodolu@gmail.com>
2021-04-29 01:43:47 +0900

    feat: mockup data 추가

---
+++ js/utils/mockup.js
@@ -0,0 +1,24 @@
+const data = [
+	{
+		id: 1,
+		heroGear: "지도자의후드",
+		type: "일반",
+		url:
+			"https://static.wikia.nocookie.net/royal-revolt-2/images/b/bc/ALeader%27sHood.png/revision/latest/scale-to-width-down/620?cb=20201020095548",
+		collection: "영웅아이템",
+		level: 3,
+		set: "나쁜산타세트",
+		number: 1,
+	},
+	{
+		id: 2,
+		heroGear: "지도자의후드",
+		type: "프로",
+		url:
+			"https://static.wikia.nocookie.net/royal-revolt-2/images/5/53/ALeader%27sHoodPro.png/revision/latest/scale-to-width-down/620?cb=20200221180708",
+		collection: "영웅아이템",
+		level: 3,
+		set: "나쁜산타세트(프로)",
+		number: 1,
+	},
+];
