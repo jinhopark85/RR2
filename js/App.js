5719f434426cc20eb33589f8b6b01b7f625791fe
ganeodolu <ganeodolu@gmail.com>
2021-05-01 23:13:31 +0900

    feat: JS 추가

--- js/App.js
+++ js/App.js
@@ -1,5 +1,14 @@
-import App from './App.js';
+import SearchInput from './SearchInput.js';
+import SearchResult from './SearchResult.js';
 
-new App({
+export default class App {
+	constructor({ keyword, $targetInput, $targetResult }) {
+    this.keyword = keyword;
+    this.$targetInput = $targetInput;
+    this.$targetResult = $targetResult;
 
-});
+    this.searchInput = new SearchInput({
+      $targetInput
+    })
+	}
+}
--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import App from "./App.js";
 
 new App({
-  
+  keyword: '',
+  $targetInput: document.querySelector('#search'),
+  $targetResult: document.querySelector('.search-result'),
 });
